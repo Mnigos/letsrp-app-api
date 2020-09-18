f9fc1ed85fe0a8d6161f03562683d94720a7e243
MoneyIgos <makowskiigor@gmail.com>
2020-09-18 18:19:38 +0200

    Change default prort to 8080

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.set('port', process.env.PORT || 6000);
+app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Listening on ${server.address().port}`);

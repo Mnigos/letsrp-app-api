862226d3b68d6ef944b1f289697677d18ea717d6
MoneyIgos <makowskiigor@gmail.com>
2020-09-21 21:37:53 +0200

    Fix applications endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/index';
+import applicationsRoutes from './routes/applications';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(routes);
+app.use(applicationsRoutes);
 
 export default app;

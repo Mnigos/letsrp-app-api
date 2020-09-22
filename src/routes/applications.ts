ea7e2919fb8880eb70b72c03428f3766e927c260
MoneyIgos <makowskiigor@gmail.com>
2020-09-22 17:40:46 +0200

    Add validation

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -2,6 +2,25 @@ import { Request, Response } from 'express';
 
 const router = require('express').Router();
 
+declare global {
+  namespace Express {
+    interface Request {
+      body: {
+        name: string;
+        date: string;
+        idea: string;
+        story: string;
+        action: string;
+        old: number;
+        know: string;
+        experience: string;
+        dc: string;
+        hex: string;
+      };
+    }
+  }
+}
+
 router.get('/applications', function (req: Request, res: Response) {
   res.status(200).send({
     message: 'Success',
@@ -9,12 +28,43 @@ router.get('/applications', function (req: Request, res: Response) {
   });
 });
 
-router.post('/applications', function (req: Request, res: Response) {
+router.post('/applications/wl', function (req: Request, res: Response) {
   const application = {
-    name: req.body.name
+    name: req.body.name as string,
+    date: req.body.date as string,
+    idea: req.body.idea as string,
+    story: req.body.story as string,
+    action: req.body.action as string,
+    old: req.body.old as number,
+    know: req.body.know as string,
+    experience: req.body.experience as string,
+    dc: req.body.dc as string,
+    hex: req.body.hex as string
   };
 
-  res.send(application);
+  const requireObjectKeysType = (
+    obj: any,
+    keys: string[],
+    expectedType: string = 'string'
+  ) => keys.every((key) => typeof obj[key] === expectedType);
+
+  res.send(
+    requireObjectKeysType(
+      req.body,
+      [
+        'name',
+        'date',
+        'idea',
+        'story',
+        'action',
+        'know',
+        'experience',
+        'dc',
+        'hex'
+      ],
+      'string'
+    )
+  );
 });
 
 export default router;

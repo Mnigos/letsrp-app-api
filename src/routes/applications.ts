428b554fdd8570204bf24ed242c88e198317c0da
Milesq <wmilosz88@gmail.com>
2020-09-29 12:43:34 +0200

    refactor: remove unexecutable code & disable one eslint rule

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -10,26 +10,13 @@ router.get('/applications', function (req: Request, res: Response) {
 });
 
 router.post('/applications/wl', function (req: Request, res: Response) {
-  const nameLength = 2;
-  const ideaLength = 20;
-  const storyLength = 200;
-  const actionLength = 50;
-  const knowLength = 10;
-  const experienceLength = 10;
-
-  const requireObjectLength = (
-    obj: any,
-    keys: string[],
-    expectedlength: string[],
-    ) => keys.every(key => obj[key].length >= expectedlength.forEach(item => {
-      return item;
-    }));
-
+  /* eslint-disable valid-typeof */
   const requireObjectKeysType = (
     obj: any,
     keys: string[],
-    expectedType: string = 'string',
+    expectedType: string = 'string'
   ) => keys.every(key => typeof obj[key] === expectedType);
+  /* eslint-enable */
 
   const validationString = requireObjectKeysType(
     req.body,

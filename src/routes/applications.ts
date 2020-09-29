2c599550c553c3adc20d0edafec3cb605f43e03f
Milesq <wmilosz88@gmail.com>
2020-09-29 12:55:47 +0200

    refactor: change old style functions to arrow functions

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -2,14 +2,14 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/applications', function (req: Request, res: Response) {
+router.get('/applications', (req: Request, res: Response) => {
   res.status(200).send({
     message: 'Success',
     status: res.statusCode
   });
 });
 
-router.post('/applications/wl', function (req: Request, res: Response) {
+router.post('/applications/wl', (req: Request, res: Response) => {
   /* eslint-disable valid-typeof */
   const requireObjectKeysType = (
     obj: any,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/', function (req: Request, res: Response) {
+router.get('/', (req: Request, res: Response) => {
   res.status(200).send({
     message: 'Success',
     status: res.statusCode

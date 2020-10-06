3dd5ddb914439092ede984a82ce8fd79f693a18a
MoneyIgos <makowskiigor@gmail.com>
2020-10-06 13:35:25 +0200

    Change literal string to chance functions

--- tests/wl.spec.ts
+++ tests/wl.spec.ts
@@ -25,26 +25,21 @@ describe('Whitelist form endpoint', () => {
       .expect(406);
   });
 
-  it('Uploading form fails when something is correct', async () => {
+  it('Uploading form accept when everything is correct', async () => {
     await request(app)
       .post('/applications/wl')
       .set('Content-Type', 'application/json')
       .send({
-        name: 'eee',
+        name: chance.name(),
         date: '01-01-2020',
-        idea:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        story:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum utviverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        action:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum utviverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        old: 1,
-        know:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        experience:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
+        idea: chance.word({ length: 20 }),
+        story: chance.word({ length: 200 }),
+        action: chance.word({ length: 50 }),
+        old: chance.age(),
+        know: chance.word({ length: 10 }),
+        experience: chance.word({ length: 10 }),
         dc: 'MoneyIgos#2000',
-        hex: '110000100000638'
+        hex: chance.string({ length: 15 })
       })
       .expect(202);
   });

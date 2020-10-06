d0e09cadadd8361ba40c2f7cffe81ddaa69a833d
MoneyIgos <makowskiigor@gmail.com>
2020-10-06 13:26:35 +0200

    Add chance library

--- tests/wl.spec.ts
+++ tests/wl.spec.ts
@@ -1,5 +1,8 @@
 import request from 'supertest';
+import Chance from 'chance';
 import app from '../src/app';
+
+const chance = Chance();
 
 describe('Whitelist form endpoint', () => {
   it('Uploading form fails when something is not correct', async () => {
@@ -8,21 +11,16 @@ describe('Whitelist form endpoint', () => {
       .post('/applications/wl')
       .set('Content-Type', 'application/json')
       .send({
-        name: 'e',
+        name: chance.age(),
         date: '01-01-2020',
-        idea:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        story:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum utviverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        action:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum utviverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        old: 1,
-        know:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
-        experience:
-          'viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut',
+        idea: chance.word({ length: 10 }),
+        story: chance.word({ length: 200 }),
+        action: chance.word({ length: 50 }),
+        old: chance.age(),
+        know: chance.word({ length: 10 }),
+        experience: chance.word({ length: 10 }),
         dc: 'MoneyIgos#2000',
-        hex: '110000100000638'
+        hex: chance.string({ length: 11 })
       })
       .expect(406);
   });

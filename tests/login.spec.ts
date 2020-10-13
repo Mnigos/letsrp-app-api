c0f873bca35becc489a4a7a59fda8efd1db3acba
MoneyIgos <makowskiigor@gmail.com>
2020-10-13 22:41:56 +0200

    Add tests for admin endpoint

---
+++ tests/login.spec.ts
@@ -0,0 +1,69 @@
+import mockingoose from 'mockingoose';
+import request from 'supertest';
+import app from '../src/app';
+import User from '../src/model/user';
+
+describe('Login system', () => {
+  beforeEach(() => {
+    mockingoose.resetAll();
+  });
+
+  it("Login fails when user data isn't provided or incomplete", async () => {
+    await request(app).post('/admin').expect(400);
+    await request(app)
+      .post('/admin')
+      .set('Content-Type', 'application/json')
+      .send({
+        user: {
+          name: 'John'
+        }
+      })
+      .expect(400);
+  });
+
+  it('login fails when user password is incorrect', async () => {
+    mockingoose(User).toReturn(
+      {
+        name: 'John',
+        pass: 'fly'
+      },
+      'findOne'
+    );
+
+    await request(app)
+      .post('/admin')
+      .set('Content-Type', 'application/json')
+      .send({
+        user: {
+          name: 'John',
+          pass: 'zaq1@WSX'
+        }
+      })
+      .expect(401);
+  });
+
+  it('login is succesfull when correct data is provided', async () => {
+    mockingoose(User).toReturn(
+      {
+        name: 'John',
+        pass: 'zaq1@WSX'
+      },
+      'findOne'
+    );
+
+    const response = await request(app)
+      .post('/admin')
+      .set('Content-Type', 'application/json')
+      .send({
+        user: {
+          name: 'John',
+          pass: 'zaq1@WSX'
+        }
+      })
+      .expect(200);
+
+    expect(response.body?.token).toMatch(
+      /^([a-zA-Z0-9-_.]+\.){2}[a-zA-Z0-9-_.]+$/i
+    );
+  });
+});

ee7a4ec43f94326fb092bffc06fdbc726fc92f71
MoneyIgos <makowskiigor@gmail.com>
2020-10-17 21:44:50 +0200

    Add ems form model

---
+++ src/model/emsForm.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IEms extends Document {
+  name: string;
+  date: string;
+  act: string;
+  bring: string;
+  action: string;
+  old: number;
+  whyU: string;
+  experience: string;
+  hoursPerDay: number;
+  dc: string;
+  hex: string;
+  formType: string;
+  status: string;
+}
+
+const EmsSchema = new Schema({
+  name: String,
+  date: String,
+  act: String,
+  bring: String,
+  action: String,
+  old: Number,
+  whyU: String,
+  experience: String,
+  hoursPerDay: String,
+  dc: String,
+  hex: String,
+  formType: String,
+  status: String
+});
+
+const EmsForm = model<IEms>('EmsForm', EmsSchema);
+
+export default EmsForm;

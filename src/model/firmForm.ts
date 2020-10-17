76af92eac1ffd0dea39411457c6968926cf530db
MoneyIgos <makowskiigor@gmail.com>
2020-10-17 19:15:15 +0200

    Add FirmForm schema

---
+++ src/model/firmForm.ts
@@ -0,0 +1,34 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IFirm extends Document {
+  name: string;
+  idea: string;
+  owner: string;
+  expects: string;
+  old: number;
+  type: string;
+  headquarters: string;
+  members: number;
+  dc: string;
+  hex: string;
+  formType: string;
+  status: string;
+}
+
+const FirmSchema = new Schema({
+  name: String,
+  idea: String,
+  owner: String,
+  expects: String,
+  old: Number,
+  headquarters: String,
+  members: Number,
+  dc: String,
+  hex: String,
+  formType: String,
+  status: String
+});
+
+const FirmForm = model<IFirm>('FirmForm', FirmSchema);
+
+export default FirmForm;

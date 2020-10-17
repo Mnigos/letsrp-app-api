d187a25d3953e5007e411de1d7a39f7030cad3df
MoneyIgos <makowskiigor@gmail.com>
2020-10-17 21:45:57 +0200

    Add lspd form model

---
+++ src/model/lspdForm.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface ILspd extends Document {
+  name: string;
+  date: string;
+  act: string;
+  bring: string;
+  action: string;
+  old: number;
+  whyU: string;
+  experience: string;
+  hoursPerDay: number;
+  dc: string;
+  hex: string;
+  formType: string;
+  status: string;
+}
+
+const LspdSchema = new Schema({
+  name: String,
+  date: String,
+  act: String,
+  bring: String,
+  action: String,
+  old: Number,
+  whyU: String,
+  experience: String,
+  hoursPerDay: String,
+  dc: String,
+  hex: String,
+  formType: String,
+  status: String
+});
+
+const LspdForm = model<ILspd>('LspdForm', LspdSchema);
+
+export default LspdForm;

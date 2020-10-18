67a5c6b6a680b267c1dbfef3b662b851489ee04e
MoneyIgos <makowskiigor@gmail.com>
2020-10-18 11:43:04 +0200

    Fix linter errors

--- typings/index.ts
+++ typings/index.ts
@@ -1,28 +1,26 @@
-declare namespace Express {
-  export interface Forms {
-    body: {
-      name?: string;
-      pass?: string;
-      about?: string;
-      owner?: string;
-      members?: string;
-      type?: string;
-      headquarters?: string;
-      expects?: string;
-      whyU?: string;
-      date?: string;
-      idea?: string;
-      story?: string;
-      action?: string;
-      old?: number;
-      act?: string;
-      bring?: string;
-      know?: string;
-      experience?: string;
-      experienceSup?: string;
-      hoursPerDay?: number;
-      dc?: string;
-      hex?: string;
-    };
-  }
+export interface Forms {
+  body: {
+    name?: string;
+    pass?: string;
+    about?: string;
+    owner?: string;
+    members?: string;
+    type?: string;
+    headquarters?: string;
+    expects?: string;
+    whyU?: string;
+    date?: string;
+    idea?: string;
+    story?: string;
+    action?: string;
+    old?: number;
+    act?: string;
+    bring?: string;
+    know?: string;
+    experience?: string;
+    experienceSup?: string;
+    hoursPerDay?: number;
+    dc?: string;
+    hex?: string;
+  };
 }
